9bd95f58ee903fa7c28827ce243d61343f0b319e
qwertyers <64732487+qwertyers@users.noreply.github.com>
2022-05-06 14:47:31 +0800

    shell scripts

--- scripts/transfer_learning.sh
+++ scripts/transfer_learning.sh
@@ -23,12 +23,3 @@ do
 
 done
 
-
-
-#        for s in 1 2 3 4 5 6 7 8 9 10:
-#        do CUDA_VISIBLE_DEVICES=0 python transde \
-#                --seed=$s
-#                --dataset=$d
-#
-#
-#        done
