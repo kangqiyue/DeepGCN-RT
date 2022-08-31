1d95661e206bb8ebceaced4c9bc34b699354eeab
yue <kqiyue@126.com>
2022-08-31 14:32:07 +0800

    clean code

--- inference.py
+++ inference.py
@@ -1,12 +1,13 @@
 import torch
 import argparse
-from models import DeeperGCN
+from models import DeeperGCN, GCNModelWithEdgeAFPreadout
 from dataset import smiles2graph, feature_to_dgl_graph,get_node_dim, get_edge_dim
 
 
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = DeeperGCN(node_in_dim=get_node_dim(), edge_in_dim=get_edge_dim(), hid_dim=200,num_layers=16, dropout=0, mlp_layers=1)
+    model = GCNModelWithEdgeAFPreadout(node_in_dim=get_node_dim(), edge_in_dim=get_edge_dim(), hidden_feats=[200] * 16,
+                                       dropout=0.1)
 
     '''load best model params'''
     # best_model_path = "/data/users/kangqiyue/kqy/DEEPGNN_RT/output/GNN_DEEPGNN_mlp1_layer_16_lr_0.001_seed_1/best_model_weight.pth"
@@ -36,7 +37,7 @@ if __name__ == '__main__':
     """
     parser = argparse.ArgumentParser(description='GNN_RT_MODEL_inference')
     parser.add_argument('--SMILES', type=str, help='SMILES of the small molecule')
-    parser.add_argument('--model_path', type=str, help='model path for DeepGNN-RT')
+    parser.add_argument('--model_path', type=str,default="model_path/best_model_weight.pth", help='model path for DeepGCN-RT')
     args = parser.parse_args()
     print(args)
 
